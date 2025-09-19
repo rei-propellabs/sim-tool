3b015f1319ebef10a3dd0c18a7179703102c0b36
Rei Boyes <180707176+rei-propellabs@users.noreply.github.com>
2025-09-19 10:00:28 -0700

    Fix styles of close tooltip button

--- src/pages/FinancialSimulationPage/stl-viewer/components/tooltip/index.tsx
+++ src/pages/FinancialSimulationPage/stl-viewer/components/tooltip/index.tsx
@@ -1,10 +1,10 @@
-import {createContext, useCallback, useContext, useEffect, useRef, useState} from "react";
+import { createContext, useCallback, useContext, useEffect, useRef, useState } from "react";
 
-import {useFrame} from "@react-three/fiber";
-import {type TooltipInterface, ViewerContext} from "../STLCanvas";
-import {BBAnchor, Billboard, Html} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { type TooltipInterface, ViewerContext } from "../STLCanvas";
+import { BBAnchor, Billboard, Html } from "@react-three/drei";
 import * as THREE from "three";
-import {ToolTipColors, TooltipIcons} from "../../stl-viewer-page";
+import { ToolTipColors, TooltipIcons } from "../../stl-viewer-page";
 // import "style.css"
 
 export interface ToolTipContext {
@@ -14,14 +14,14 @@ export interface ToolTipContext {
 }
 
 export const ToolTipContext = createContext<ToolTipContext>({
-    position: {x: 0, y: 0, z: 0},
+    position: { x: 0, y: 0, z: 0 },
     hoverState: false,
     clickedState: false
 } as ToolTipContext);
 
 export default function ToolTip(toolTipData: TooltipInterface) {
 
-    const {config, dispatch} = useContext(ViewerContext);
+    const { config, dispatch } = useContext(ViewerContext);
     const showToolTip = useCallback(() => config.showToolTipInPlanOnly ? config.mode === 'plan' : true, [config.mode])
 
     const [hoverState, setHoverState] = useState(false);
@@ -55,7 +55,7 @@ export default function ToolTip(toolTipData: TooltipInterface) {
     const toolTipColor: string = ToolTipColors.levels[getToolTipLevel(toolTipData.Gold_vol)].color;
 
 
-    let formatter = Intl.NumberFormat('en', {notation: 'compact'});
+    let formatter = Intl.NumberFormat('en', { notation: 'compact' });
     let revenue = formatter.format(toolTipData.net_value);
 
 
@@ -67,15 +67,15 @@ export default function ToolTip(toolTipData: TooltipInterface) {
         }}>
 
             <mesh ref={meshRef} position={[toolTipData.position.x, toolTipData.position.y, toolTipData.position.z]}
-                  onClick={showToolTip() ? flipClick : () => {
-                  }}
-                  onPointerEnter={showToolTip() ? flipHover : () => {
-                  }} onPointerLeave={showToolTip() ? flipHover : () => {
-            }} castShadow>
-                <octahedronGeometry args={[0.5]}/>
+                onClick={showToolTip() ? flipClick : () => {
+                }}
+                onPointerEnter={showToolTip() ? flipHover : () => {
+                }} onPointerLeave={showToolTip() ? flipHover : () => {
+                }} castShadow>
+                <octahedronGeometry args={[0.5]} />
                 <meshPhysicalMaterial side={2} transparent
-                                      metalness={1.0} roughness={0.65}
-                                      color={toolTipColor}/>
+                    metalness={1.0} roughness={0.65}
+                    color={toolTipColor} />
             </mesh>
             {
                 clickedState && showToolTip() &&
@@ -85,10 +85,10 @@ export default function ToolTip(toolTipData: TooltipInterface) {
                         position={[toolTipData.position.x, toolTipData.position.y, toolTipData.position.z]}
                     >
                         <mesh ref={ringRef}>
-                            <ringGeometry args={[0.8, 1.0, 4]}/>
+                            <ringGeometry args={[0.8, 1.0, 4]} />
                             <meshPhysicalMaterial side={2} transparent
-                                                  metalness={1.0} roughness={0.65}
-                                                  color={toolTipColor}/>
+                                metalness={1.0} roughness={0.65}
+                                color={toolTipColor} />
                         </mesh>
                         <BBAnchor anchor={[1, 1, 0]}>
                             <Html position={[0.5, 0.5, 0]} center>
@@ -98,10 +98,10 @@ export default function ToolTip(toolTipData: TooltipInterface) {
                                         <p> BH-{
                                             toolTipData.serial
                                         }</p>
-                                        <button
-                                            onClick={flipClick}>
-                                            <img loading={'eager'} src={TooltipIcons.close} className={'size-3 text-tooltip-text '}/>
-                                        </button>
+                                        <div
+                                            className="bg-transparent p-0 cursor-pointer" onClick={flipClick}>
+                                            <img loading={'eager'} src={TooltipIcons.close} className={'size-3 text-tooltip-text '} />
+                                        </div>
 
                                     </div>
                                     <div className={'flex flex-row  items-center gap-1 font-normal text-xs '}>
@@ -110,19 +110,19 @@ export default function ToolTip(toolTipData: TooltipInterface) {
                                                 background: ToolTipColors.levels[getToolTipLevel(toolTipData.Gold_vol)].color,
                                                 border: `2px solid ${ToolTipColors.levels[getToolTipLevel(toolTipData.Gold_vol)].border}`
                                             }}
-                                            className={`size-3 rounded-full`}/>
+                                            className={`size-3 rounded-full`} />
                                         <p>{toolTipData.Gold_vol.toPrecision(4)}{" "} g/t Au</p>
                                     </div>
                                     <div className={'flex flex-row items-center gap-1 font-normal text-xs'}>
-                                        <img loading={'eager'} src={TooltipIcons.delete} className={'size-3'}/>
+                                        <img loading={'eager'} src={TooltipIcons.delete} className={'size-3'} />
                                         <p>{toolTipData.Waste_Vol.toPrecision(4)} g/t</p>
                                     </div>
                                     <div className={'flex flex-row items-center gap-1 font-normal text-xs '}>
-                                        <img loading={'eager'} src={TooltipIcons.arrows_vertical} className={'size-3'}/>
+                                        <img loading={'eager'} src={TooltipIcons.arrows_vertical} className={'size-3'} />
                                         <p>{toolTipData.depth.toPrecision(4)} g/t</p>
                                     </div>
                                     <div className={'flex flex-row items-center gap-1 font-normal text-xs '}>
-                                        <img loading={'eager'} src={TooltipIcons.currency_dollar} className={'size-3'}/>
+                                        <img loading={'eager'} src={TooltipIcons.currency_dollar} className={'size-3'} />
                                         <p>{revenue}</p>
                                     </div>
 
