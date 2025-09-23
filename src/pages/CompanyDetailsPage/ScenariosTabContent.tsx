d8e88bf8c89d0cbe3bf05ca42a3a3c9e0b220acc
Rei Boyes <180707176+rei-propellabs@users.noreply.github.com>
2025-09-23 14:59:47 -0700

    Add scenario table

--- src/pages/CompanyDetailsPage/ScenariosTabContent.tsx
+++ src/pages/CompanyDetailsPage/ScenariosTabContent.tsx
@@ -1,15 +1,42 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./ScenariosTabContent.module.css"
 import { SmallLoadingSpinner } from "components/SmallLoadingSpinner/SmallLoadingSpinner";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NoItemsText } from "components/NoItemsText/NoItemsText";
+import { ColumnConfig, NavigationTable } from "components/NavigationTable/NavigationTable";
+import { Presentation } from "images/Dynamic/Presentation";
+import { Edit } from "images/Dynamic/Edit";
+import navTableStyles from "components/NavigationTable/NavigationTable.module.css";
 
 export const ScenariosTabContent = () => {
+
+  interface PresentationTableRow {
+    name: string;
+    dateCreated: string;
+    lastUpdated: string;
+    action: string;
+  }
   const NUM_ROWS = 10;
 
   const query = new URLSearchParams(useLocation().search);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [numRows, setNumRows] = useState<number>(0);
+  const [numRows, setNumRows] = useState<number>(1);
+  const [orgTableData, setOrgTableData] = useState<PresentationTableRow[]>([]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setOrgTableData(
+        [
+          {
+            name: "Presentation 1",
+            dateCreated: "04/04/2024",
+            lastUpdated: "04/04/2024",
+            action: ""
+          },
+        ]
+      );
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return (
@@ -20,6 +47,68 @@ export const ScenariosTabContent = () => {
         </div>
 
       </div>
+    )
+  }
+
+  const orgColumns: ColumnConfig<PresentationTableRow>[] = [
+    {
+      key: 'name', header: 'Company',
+    },
+    // { key: 'lastUploadAt', header: 'Last upload' },
+    {
+      key: 'dateCreated', header: 'Date Created',
+    },
+    { key: 'lastUpdated', header: 'Last Updated' },
+    {
+      key: 'actions',
+      header: 'Actions',
+      render: (scenario) => {
+        // if (scenario.uploadCount === 0) return null;
+        return (
+          <button
+            className={"primary-button"}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}>
+            <Presentation size={16} />
+            Launch Presentation
+          </button>
+        );
+      },
+    },
+    { key: 'menu', header: 'Menu' },
+
+  ];
+
+
+  const table = () => {
+    return (
+      <NavigationTable
+        expandIndexes={[0, 1, 2, 3]}
+        columns={orgColumns}
+        data={orgTableData}
+        onRowClick={(index) => {
+          // localStorage.setItem("company", JSON.stringify(organizations[index]));
+
+          // navigate(`/admin/c?t=${organizations[index].uploadCount === 0 ? 0 : 1}&orgId=${organizations[index].id}`);
+        }}
+      renderMenu={(rowIndex, closeMenu) => (
+        <div>
+          <button
+            className={`buttonNoBg 
+              ${navTableStyles.option} ${navTableStyles.editOption}`
+            }
+            onClick={(e) => {
+              e.preventDefault();
+              // navigate(`/admin/c?orgId=${organizations[rowIndex].id}`);
+            }}
+          >
+            <Edit size={16}/>
+            Edit
+          </button>
+        </div>
+      )}
+      />
     )
   }
 
@@ -34,12 +123,7 @@ export const ScenariosTabContent = () => {
             onActionButtonClick={() => { }}
           />
         ) :
-          (
-            <>
-
-              <SmallLoadingSpinner />
-            </>
-          )
+          table()
       }
 
     </div >
